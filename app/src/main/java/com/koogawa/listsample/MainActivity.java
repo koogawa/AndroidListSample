7a9fb374b414ab420a55408c066dead0164cb381
Kosuke Ogawa <koogawa.app@gmail.com>
2017-02-20 17:40:20 +0900

    Add ListView

--- app/src/main/java/com/koogawa/listsample/MainActivity.java
+++ app/src/main/java/com/koogawa/listsample/MainActivity.java
@@ -2,12 +2,69 @@ package com.koogawa.listsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView lv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] members = { "mhidaka", "rongon_xp", "kacchi0516", "kobashinG",
+                "seit", "kei_i_t", "furusin_oriver" };
+
+        lv = (ListView) findViewById(R.id.listView1);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_expandable_list_item_1, members);
+
+        lv.setAdapter(adapter);
+
+        //リスト項目がクリックされた時の処理
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                ListView listView = (ListView) parent;
+                String item = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), item + " clicked",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        lv.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            //リスト項目が選択された時の処理
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ListView listView = (ListView) parent;
+                String item = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), item + " selected",
+                        Toast.LENGTH_LONG).show();
+            }
+            //リスト項目がなにも選択されていない時の処理
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(getApplicationContext(), "no item selected",
+                        Toast.LENGTH_LONG).show();
+            }
+        });
+
+        //リスト項目が長押しされた時の処理
+        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                ListView listView = (ListView) parent;
+                String item = (String) listView.getItemAtPosition(position);
+                Toast.makeText(getApplicationContext(), item + " long clicked",
+                        Toast.LENGTH_LONG).show();
+                return false;
+            }
+        });
     }
 }
